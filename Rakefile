1715a53e1987f4bea52d0044523f433f6328ab8d
justinmcla <jwmclawhorn@gmail.com>
2020-08-11 12:31:40 -0400

    Done.

--- Rakefile
+++ Rakefile
@@ -3,5 +3,10 @@ ENV["SINATRA_ENV"] ||= "development"
 require_relative './config/environment'
 require 'sinatra/activerecord/rake'
 
+namespace :db do
+    task :load_config do
+        require './app/controllers/application_controller'
+    end
+end
 
 # Type `rake -T` on your command line to see the available rake tasks.
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < Sinatra::Base
   set :views, proc { File.join(root, '../views/') }
   register Sinatra::Twitter::Bootstrap::Assets
+  register Sinatra::ActiveRecordExtension
+
+  set :database, {:adapter => "sqlite3", :database => "db/development.sqlite3"}
 
   get '/' do
     erb :"application/index"
